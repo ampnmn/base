635f71449b7009376b1156ecd697710b244cb8c0
ampnmn <dev.ampnmn@gmail.com>
2019-09-22 01:31:34 +0900

    make non-static

--- src/main/java/com/anpnmn/base/repository/impl/MessageRepositoryImpl.java
+++ src/main/java/com/anpnmn/base/repository/impl/MessageRepositoryImpl.java
@@ -12,7 +12,7 @@ import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 
 public class MessageRepositoryImpl implements MessageRepository {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(MessageRepositoryImpl.class);
+    private final Logger logger = LoggerFactory.getLogger(MessageRepositoryImpl.class);
 
     private final NamedParameterJdbcTemplate jdbcTemplate;
 
@@ -32,7 +32,7 @@ public class MessageRepositoryImpl implements MessageRepository {
         } catch (EmptyResultDataAccessException e) {
             return null;
         } catch (DataAccessException e) {
-            LOGGER.error("DataAccessError!!!");
+            logger.error("DataAccessError!!!");
             return null;
         }
     }
